1faecb521b1c37fe4b1efc88f56b0f6a366e3504
Jakob Löw <jakob@m4gnus.de>
2019-02-17 19:44:07 +0100

    initial commit

---
+++ bot.py
@@ -0,0 +1,29 @@
+import os, json
+from telegram.ext import Updater, CommandHandler
+
+with open("./config.json", "r") as fd:
+	config = json.load(fd)
+
+taunts = {}
+commands = []
+for file in os.listdir(config["taunt-dir"]):
+	num = int(file.split(" ")[0])
+	path = os.path.join(config["taunt-dir"], file)
+	taunts[num] = path
+	commands.append(str(num))
+
+def handleCmd(bot, update):
+	text = update.message.text.split(" ")[0][1 : ]
+	num = int(text)
+	file = taunts[num]
+
+	with open(file, "rb") as fd:
+		update.message.reply_voice(fd)
+
+updater = Updater(config["telegram-token"])
+
+for cmd in commands:
+	updater.dispatcher.add_handler(CommandHandler(cmd, handleCmd))
+
+updater.start_polling()
+updater.idle()
